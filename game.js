ce785b31f9d76f3a328c07913a63e4abbbd52665
Caleb Yearsley <cnyearsley@outlook.com>
2017-05-19 18:57:41 -0600

    render some stuff

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
 
-var game = new Phaser.Game('100%', '100%', Phaser.AUTO, '', { preload: preload, create: create, update: update });
+let game = new Phaser.Game('100%', '100%', Phaser.AUTO, '', { preload: preload, create: create, update: update });
 let gameCanvas, body;
+
+let level = require('./levels/levels/start.js').level;
+//parse level
+let parsedLevel = '';
+parsedLevel = level.join('\n');
 
 function preload() {
     gameCanvas = document.getElementsByTagName('canvas');
@@ -14,9 +19,33 @@ function preload() {
 
     });
     game.scale.refresh();
+
+    //Images
+    game.load.image('tiles', 'assets/grass.png');
 }
 
 function create() {
+
+    //  Add data to the cache
+    console.log("PARSED LEVEL: ", parsedLevel);
+    game.cache.addTilemap('startLevel', null, parsedLevel, Phaser.Tilemap.CSV);
+
+    //  Create our map (the 16x16 is the tile size)
+    let map = game.add.tilemap('startLevel', 10, 10);
+
+    //  'tiles' = cache image key, 10x10 = tile size
+    map.addTilesetImage('tiles', 'tiles', 10, 10);
+
+    //  0 is important
+    layer = map.createLayer(0);
+
+    //  Scroll it
+    layer.resizeWorld();
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    cursors = game.input.keyboard.createCursorKeys();
+
 }
 
 function update() {
