ed904e56b39ea4a72656064bd1aa464a9a18072c
Caleb Yearsley <cnyearsley@outlook.com>
2017-05-19 18:15:44 -0600

    Feature: Add level config and level files/dirs. Fix: Add 'phaser-ce' to package.json

---
+++ levels/levels-config/tile-config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    tiles: {
+        '1a': 'grass.png',
+        '1b': 'bush.png',
+        '1c': 'LeftPath.png',
+        '1d': 'Path.png',
+        '1e': 'RightPath.png',
+        '1f': 'Sign.png',
+        '1g': 'tree-1x2.png'
+    }
+};
